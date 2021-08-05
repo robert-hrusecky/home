e8d23752f56938d697b1a59c0ed1a2b535e599ab
Robert Hrusecky <robert.hrusecky@utexas.edu>
2021-08-05 15:12:29 -0500

    Thu Aug  5 03:12:29 PM CDT 2021

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-find -mindepth 1 -maxdepth 1 | sed 's%./\([^/]\+\)%\1%' | xargs -n1 -I% sh -c "if [ '%' != '.git' ] && [ '%' != 'install.sh' ]; then echo Linking '%'; ln -sf $PWD/% ~/%; fi"
+# find -mindepth 1 -maxdepth 1 | sed 's%./\([^/]\+\)%\1%' | xargs -n1 -I% sh -c "if [ '%' != '.git' ] && [ '%' != 'install.sh' ]; then echo Linking '%'; echo ln -sf $PWD/% ~/%; fi"
+
+find -type f | sed 's%\(.*\)/\([^/]*\)%echo Linking \2; echo mkdir -p \1 && echo ln -s \0 ~/\0%' | tr '\n' '\0' | xargs -0 -n1 sh -c
